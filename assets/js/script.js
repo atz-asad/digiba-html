8fd2799dcd0e20db34b7ca3f4bcd4bfcacd4fd23
asad <ashad731386@gmail.com>
2024-08-13 13:08:11 +0600

    motamoti done

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,25 +90,25 @@
     /--------------------------------------------------------*/
 
     // menu-toggler
-    document.addEventListener('DOMContentLoaded', function() {
-        var menuIcon = document.querySelector('.menu-toggler');
-        var offcanvas = document.getElementById('mobilemenu-offcanvas');
+    // document.addEventListener('DOMContentLoaded', function() {
+    //     var menuIcon = document.querySelector('.menu-toggler');
+    //     var offcanvas = document.getElementById('mobilemenu-offcanvas');
 
-        // Add event listener for when the offcanvas is shown
-        offcanvas.addEventListener('shown.bs.offcanvas', function() {
-            menuIcon.classList.add('active');
-        });
+    //     // Add event listener for when the offcanvas is shown
+    //     offcanvas.addEventListener('shown.bs.offcanvas', function() {
+    //         menuIcon.classList.add('active');
+    //     });
 
-        // Add event listener for when the offcanvas is hidden
-        offcanvas.addEventListener('hidden.bs.offcanvas', function() {
-            menuIcon.classList.remove('active');
-        });
+    //     // Add event listener for when the offcanvas is hidden
+    //     offcanvas.addEventListener('hidden.bs.offcanvas', function() {
+    //         menuIcon.classList.remove('active');
+    //     });
 
-        // Optionally, you can keep the original toggle functionality on menuIcon click
-        menuIcon.addEventListener('click', function() {
-            this.classList.toggle('active');
-        });
-    });
+    //     // Optionally, you can keep the original toggle functionality on menuIcon click
+    //     menuIcon.addEventListener('click', function() {
+    //         this.classList.toggle('active');
+    //     });
+    // });
 
 
     if ($('.mobileMenu').length > 0) {
@@ -158,7 +158,6 @@
     // // 2. Register GSAP
     gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
     // /////////////////////////////////////////////////////
-
 
     /////////////////////////////////////////////////////
     // 3. Config GSAP
@@ -318,6 +317,9 @@
         });
     };
 
+    $(document).ready(function() {
+        $('select').niceSelect();
+    });
 
 
 })(jQuery);
